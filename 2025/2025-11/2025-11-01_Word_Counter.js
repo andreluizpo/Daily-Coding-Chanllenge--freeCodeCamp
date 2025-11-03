91f7eb65b6551dbf4563a36e6273bdd38fc95441
André Luiz Pereira de Oliveira <156046877+andreluizpo@users.noreply.github.com>
2025-11-03 15:40:21 -0400

    Create 2025-11-01_Word_Counter.js

---
+++ 2025/2025-11/2025-11-01_Word_Counter.js
@@ -0,0 +1,19 @@
+/*
+freeCodeCamp - Daily Coding Challenges (11/01/2025) 
+
+*Word Counter*
+Given a sentence string, return the number of words that are in the sentence.
+
+- Words are any sequence of non-space characters and are separated by a single space.
+*/
+
+function countWords(sentence) {
+    const words = sentence.trim().split(/\s+/);
+    return words.length;
+}
+
+countWords("Hello world"); // should return 2.
+countWords("The quick brown fox jumps over the lazy dog."); // should return 9.
+countWords("I like coding challenges!"); // should return 4.
+countWords("Complete the challenge in JavaScript and Python."); // should return 7.
+countWords("The missing semi-colon crashed the entire internet."); // should return 7.
